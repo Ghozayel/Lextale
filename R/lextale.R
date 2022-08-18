cf821b8e5c0332a2f1ade3c3ebc3ced4ba00ec46
Ghozayel <gmo6606@gmail.com>
2022-08-18 13:00:08 +0100

    R folder added with a draft r code, dataset.r updated

---
+++ R/lextale.R
@@ -0,0 +1,17 @@
+lextale <- function(lextale) {
+  #marking correct/incorrect entries (1=correct and 0=incorrect) in a new column called score
+  lextale$score <- ifelse(lextale$answer==lextale$correct, 1,0) %>%
+    #calculate score based on the marked file
+    score <- markedLextale %>%
+      group_by(ID,type) %>%
+      summarise(N.correct=sum(score)) %>%
+      mutate(p.correct=if_else(type=="word",N.correct/40*100, N.correct/20*100))
+
+score.final <-
+  score%>%
+  group_by(ID)%>%
+  summarise(finalScore=mean(p.correct))
+
+#saving the file
+write.csv(score.final, 'multiL2/lextaleFinalScore.csv')
+}
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,10 +3,13 @@ library(tidyverse)
 library(here)
 library(janitor)
 
-lexData <- read.delim(here('data-raw', 'lextale-fakeData.csv'), sep =';')
-ids <- gl(30, 60)
+fjords <- read.csv(here('data-raw', 'lextale-fakeData.csv')) # Read in csv file
+
+ids <- gl(30, 60) #generate new variable 'ids'
 ids
-answer <- gl(30, 60,0/1)
+dplyr::full_join(ids, lexData, by = )
+
+answer <- gl(30, 60,0/1) #generate new variable for answers
 
 
 usethis::use_data(DATASET, overwrite = TRUE)
