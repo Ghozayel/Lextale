7c60522579fc9d4cb5a778f7a8e88dae2e19250b
Ghozayel <gmo6606@gmail.com>
2022-08-20 12:44:35 +0100

    updated function codes and data in dataset.r

--- R/lextale.R
+++ R/lextale.R
@@ -1,6 +1,6 @@
 
 
-#' Title
+#' Title LexTALE % correctAV
 #'
 #' @param lextale
 #'
@@ -8,21 +8,12 @@
 #' @export
 #'
 #' @examples
-lextale <- function(lextale, var) {
-
-  #marking correct/incorrect entries (1=correct and 0=incorrect) in a new column called score
-  lextale$score <- ifelse(lextale$answer==lextale$correct, 1,0) %>%
-    #calculate score based on the marked file
-    score <- markedLextale %>%
-      group_by(ID,type) %>%
-      summarise(N.correct=sum(score)) %>%
-      mutate(p.correct=if_else(type=="word",N.correct/40*100, N.correct/20*100))
-
-score.final <-
-  score%>%
-  group_by(ID)%>%
-  summarise(finalScore=mean(p.correct))
-
-#saving the file
-write.csv(score.final, 'multiL2/lextaleFinalScore.csv')
+lextale <- function(var, n = ,...) {
+  data$score <- ifelse(data$var ==data$correct, 1,0) %>% #marking correct/incorrect entries (1=correct and 0=incorrect) in a new column called score
+    group_by(ID,type) %>%     #calculate number of correct answers per type (word/ non-word) for each participant
+    summarise(N.correct=sum(score)) %>%
+    mutate(p.correct=if_else(type=="word",N.correct/40*100, N.correct/20*100)) %>%     #p.correct is the percentage
+    group_by(ID)%>%
+    summarise(finalScore=mean(p.correct)) %>%
+    write.csv(here('results', 'lextaleFinalScore.csv')) #saving the file
 }
