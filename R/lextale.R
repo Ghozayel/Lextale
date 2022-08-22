8061d63f399f6e9edb73b04da5df85cc810557a3
Ghozayel <gmo6606@gmail.com>
2022-08-22 05:57:11 +0100

    function code done

--- R/lextale.R
+++ R/lextale.R
@@ -3,17 +3,21 @@
 #' @param var A lextale var must be provided as a numeric data, either 0 or 1, with 60 responses at least for each participant.
 #' @export
 #' @examples
-#' lextale(0, n = 60)
-lextale <- function(var, n = min(60)) {
-  stopifnot("Input must be numeric" = is.numeric(var))
-  stopifnot("Input must be logical" = is.logical(var))
-  lextale$score <- ifelse(lextale$var ==lextale$correct, 1,0) %>% #marking correct/incorrect entries (1=correct and 0=incorrect) in a new column called score
-    group_by(ID,type) %>%     #calculate number of correct answers per type (word/ non-word) for each participant
+#' lextale(c(0,1,0,0,1,0,1,0,1,0,0,0,1,1,1,0,0,0,1,0,0,1,0,0,1,0,1,0,1,0,0,0,1,1,1,0,0,0,1,0,0,1,0,0,1,0,1,0,1,0,0,0,1,1,1,0,0,0,1,0))
+lextale <- function(answerVar) {
+  stopifnot("Input must be numeric" = is.numeric(answerVar))
+    if (length(answerVar)!=60){
+      warning("Please make sure that you provided 60 data points/rows/responses.")
+    }
+  data <- read_csv('data-raw/lextale-sampleData.csv')
+  input <- c(answerVar)
+  result <- cbind(data, input)
+  score <- c(ifelse(result$input ==result$correct, 1,0)) #marking correct/incorrect entries (1=correct and 0=incorrect) in a new column called score
+  result <- cbind(result, score)
+  result <- result %>%
+    group_by(type) %>%     #calculate number of correct answers per type (word/ non-word) for each participant
     summarise(N.correct=sum(score)) %>%
-    mutate(p.correct=if_else(type=="word",N.correct/40*100, N.correct/20*100)) %>%     #p.correct is the percentage for each type
-    group_by(ID)%>%
-    summarise(finalScore=mean(p.correct)) %>% #averaging the correct results of the two type to get the lextale score
-    print(finalScore) %>%
-    write.csv(here::here('results', 'lextaleScore.csv') #saving the file
-    )
+    mutate(p.correct=if_else(type=="word",N.correct/40*100, N.correct/20*100))     #p.correct is the percentage for each type
+  finalScore <- mean(result$p.correct) #averaging the correct results of the two type to get the lextale score
+  return(finalScore)
 }
