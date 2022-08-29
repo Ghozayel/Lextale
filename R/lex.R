baedc2977aae66abc97c4dfe55c0e094b25399f2
Ghozayel <gmo6606@gmail.com>
2022-08-29 14:46:09 +0100

    removing read.csv from the codes

--- R/lex.R
+++ R/lex.R
@@ -3,30 +3,67 @@
 #' @param answerVar a 0/1 values responses to lextale test
 #' @export
 #' @examples
-#' lex(sample(c(0,1), replace = TRUE, 600))
+#' lex(sample(c(0,1), replace = TRUE, 120))
 #' @name lex
-
-library(tidyverse)
-library(here)
-
 lex <- function(answerVar) {
-  stopifnot("Input must be numeric" = is.numeric(answerVar))
-  if (length(answerVar)!=600){
-    warning("Please make sure that you provided responses for 10 participants, 60 data-ponits/responses for each, a total of 600 rows.")
-  }
-  data <- readr::read_csv(here('data-raw','lextale-sampleData.csv'))
-  ids <- gl(10, 60) #generate 10 ids
+  #stopifnot("Input must be numeric" = is.numeric(answerVar))
+  #if (length(answerVar)!=120){
+    #warning("Please make sure that you provided 120 responses/data-ponits/rows.")
+  #}
+  #numeric vector
+  ids <- gl(2, 60) #generate 2 ids
+  #logical vector
+  correct <- c(0,1,1,1,0,1,1,1,1,0,
+              1,1,1,1,1,0,1,1,0,1,
+              1,1,0,1,1,1,0,1,1,0,
+              1,0,0,1,1,1,0,1,1,0,
+              1,0,0,1,1,1,1,0,0,1,
+              1,1,1,0,0,1,0,0,1,1,
+              0,1,1,1,0,1,1,1,1,0,
+              1,1,1,1,1,0,1,1,0,1,
+              1,1,0,1,1,1,0,1,1,0,
+              1,0,0,1,1,1,0,1,1,0,
+              1,0,0,1,1,1,1,0,0,1,
+              1,1,1,0,0,1,0,0,1,1)
+  #character vector
+  type <- c('nonword','word','word','word','nonword','word',
+            'word','word','word','nonword','word','word',
+            'word','word','word','nonword','word','word',
+            'nonword','word','word','word','nonword','word',
+            'word','word','nonword','word','word','nonword',
+            'word','nonword','nonword','word','word','word',
+            'nonword','word','word','nonword','word','nonword',
+            'nonword','word','word','word','word','nonword',
+            'nonword','word','word','word','word','nonword',
+            'nonword','word','nonword','nonword','word','word',
+            'nonword','word','word','word','nonword','word',
+            'word','word','word','nonword','word','word',
+            'word','word','word','nonword','word','word',
+            'nonword','word','word','word','nonword','word',
+            'word','word','nonword','word','word','nonword',
+            'word','nonword','nonword','word','word','word',
+            'nonword','word','word','nonword','word','nonword',
+            'nonword','word','word','word','word','nonword',
+            'nonword','word','word','word','word','nonword',
+            'nonword','word','nonword','nonword','word','word')
+  #logical vector
   input <- c(answerVar)
-  result <- cbind(data, input, ids)
-  score <- c(ifelse(result$input ==result$correct, 1,0)) #marking correct/incorrect entries (1=correct and 0=incorrect) in a new column called score
-  result <- cbind(result, score)
-  result <- result %>%
-    dplyr::group_by(ids,type) %>%     #calculate number of correct answers per type (word/ non-word) for each participant
+  result <- cbind(ids, correct, type, input)
+  #marking correct/incorrect entries (1=correct and 0=incorrect) in a new column called score
+  score <- c(ifelse(result$input == result$correct, 1,0))
+  result1 <- cbind(result, score)
+  #print(result1)
+  result2 <- result1 %>%
+    #calculate number of correct answers per type (word/ non-word) for each participant
+    dplyr::group_by(ids,type) %>%
     dplyr::summarise(N.correct=sum(score)) %>%
-    dplyr::mutate(p.correct=if_else(type=="word", N.correct/40*100, N.correct/20*100))     #p.correct is the percentage for each type
-  result <- result %>%
+    #p.correct is the percentage for each type
+    dplyr::mutate(p.correct=if_else(type=="word", N.correct/40*100, N.correct/20*100))
+  print(result2)
+  result3 <- result2 %>%
     dplyr::group_by(ids) %>%
-    dplyr::summarise(p.correctAV = mean(result$p.correct)) #averaging the correct results of the two types to get the lextale score
-  print(result)
-  return(result)
+    #averaging the correct results of the two types to get the lextale score
+    dplyr::summarise(p.correctAV = mean(result$p.correct))
+  write.csv(here::here('result.csv'), result3)
+  return(result3)
 }
