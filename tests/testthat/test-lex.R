28ac6c5cffdce3ed533be61f4bc8e481ad96f662
Ghozayel <gmo6606@gmail.com>
2022-09-05 21:45:32 +0100

    testthat

--- tests/testthat/test-lex.R
+++ tests/testthat/test-lex.R
@@ -1,3 +1,6 @@
-test_that("charachter input causes error", {
-  expect_error(lextale::lex('cat'))
+test_that("is_tibble", {
+  expect_false(is_tibble(trees))
+  expect_true(is_tibble(as_tibble(trees)))
+  expect_false(is_tibble(NULL))
+  expect_false(is_tibble(0))
 })
