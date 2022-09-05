dfee8aeaeb71ff7b07876a1f8145d8006aa1c0c2
Ghozayel <gmo6606@gmail.com>
2022-09-05 20:58:46 +0100

    tibble as an argument

--- R/lex.R
+++ R/lex.R
@@ -1,69 +1,23 @@
 #' @title calculate lextale score
-#' @description calculate the lextale score
-#' @param answerVar a 0/1 values responses to lextale test
+#' @description to calculate lextale score, provided with a tibble of at least four variables
+#' @param answerVar a tibble with ids,type (word/nonword),correct(0/1) and answer(0/1) variables
 #' @export
-#' @examples
-#' lex(sample(c(0,1), replace = TRUE, 120))
 #' @name lex
 lex <- function(answerVar) {
-  #stopifnot("Input must be numeric" = is.numeric(answerVar))
-  #if (length(answerVar)!=120){
-    #warning("Please make sure that you provided 120 responses/data-ponits/rows.")
-  #}
-  #numeric vector
-  ids <- gl(2, 60) #generate 2 ids
-  #logical vector
-  correct <- c(0,1,1,1,0,1,1,1,1,0,
-              1,1,1,1,1,0,1,1,0,1,
-              1,1,0,1,1,1,0,1,1,0,
-              1,0,0,1,1,1,0,1,1,0,
-              1,0,0,1,1,1,1,0,0,1,
-              1,1,1,0,0,1,0,0,1,1,
-              0,1,1,1,0,1,1,1,1,0,
-              1,1,1,1,1,0,1,1,0,1,
-              1,1,0,1,1,1,0,1,1,0,
-              1,0,0,1,1,1,0,1,1,0,
-              1,0,0,1,1,1,1,0,0,1,
-              1,1,1,0,0,1,0,0,1,1)
-  #character vector
-  type <- c('nonword','word','word','word','nonword','word',
-            'word','word','word','nonword','word','word',
-            'word','word','word','nonword','word','word',
-            'nonword','word','word','word','nonword','word',
-            'word','word','nonword','word','word','nonword',
-            'word','nonword','nonword','word','word','word',
-            'nonword','word','word','nonword','word','nonword',
-            'nonword','word','word','word','word','nonword',
-            'nonword','word','word','word','word','nonword',
-            'nonword','word','nonword','nonword','word','word',
-            'nonword','word','word','word','nonword','word',
-            'word','word','word','nonword','word','word',
-            'word','word','word','nonword','word','word',
-            'nonword','word','word','word','nonword','word',
-            'word','word','nonword','word','word','nonword',
-            'word','nonword','nonword','word','word','word',
-            'nonword','word','word','nonword','word','nonword',
-            'nonword','word','word','word','word','nonword',
-            'nonword','word','word','word','word','nonword',
-            'nonword','word','nonword','nonword','word','word')
-  #logical vector
-  input <- c(answerVar)
-  result <- cbind(ids, correct, type, input)
   #marking correct/incorrect entries (1=correct and 0=incorrect) in a new column called score
-  score <- c(ifelse(result$input == result$correct, 1,0))
-  result1 <- cbind(result, score)
-  #print(result1)
-  result2 <- result1 %>%
+  answerVar$score <- c(ifelse(answerVar$answer == answerVar$correct, 1,0))
+  print(answerVar)
+  result <- answerVar %>%
     #calculate number of correct answers per type (word/ non-word) for each participant
     dplyr::group_by(ids,type) %>%
     dplyr::summarise(N.correct=sum(score)) %>%
     #p.correct is the percentage for each type
     dplyr::mutate(p.correct=if_else(type=="word", N.correct/40*100, N.correct/20*100))
-  print(result2)
-  result3 <- result2 %>%
+  print(result)
+  result2 <- result %>%
     dplyr::group_by(ids) %>%
     #averaging the correct results of the two types to get the lextale score
     dplyr::summarise(p.correctAV = mean(result$p.correct))
-  write.csv(here::here('result.csv'), result3)
-  return(result3)
+  write.csv(here::here('result.csv'), result2)
+  return(result2)
 }
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,13 +1,8 @@
 ## code to prepare `DATASET` dataset goes here
 library(tidyverse)
 library(here)
-library(janitor)
 
-lextale <- read.csv(here('data-raw', 'lextale-sampleData.csv')) # Read in csv file
-ids <- gl(10, 60) #generate new variable 'ids'
-answer <- sample(c(0,1), replace = TRUE, 600) #generate new variable of binary random answers
-is.numeric(answer)
-lextale <- lextale %>% #combine the generated 2 variables into the lextale data
-  dplyr::mutate(answer = answer) %>%
-  dplyr::mutate(ids = ids)
+lextale <- read.csv(here('data-raw', 'lextaledata.csv')) # Read in csv file
+lextale$answer <- sample(c(0,1), replace = TRUE, 600) #generate new variable of binary random answers
+is.numeric(lextale$answer)
 usethis::use_data(lextale, overwrite = TRUE)
