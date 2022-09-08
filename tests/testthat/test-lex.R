d3c9b76197aff3d5ebc6c241a21a3fe969b4dffa
Ghozayel <gmo6606@gmail.com>
2022-09-08 21:23:03 +0100

    You are a coding rockstar!

--- tests/testthat/test-lex.R
+++ tests/testthat/test-lex.R
@@ -1,6 +1,3 @@
-test_that("is_tibble", {
-  expect_false(is_tibble(trees))
-  expect_true(is_tibble(as_tibble(trees)))
-  expect_false(is_tibble(NULL))
-  expect_false(is_tibble(0))
+test_that("charachter input causes error", {
+  expect_error(Lextale::lex(sample(c(Yes/No), replace = TRUE, 600)))
 })
