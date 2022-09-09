ed284fa7e8e5ec905f315178a9ca832eb944278c
Ghozayel <gmo6606@gmail.com>
2022-09-09 18:41:28 +0100

    expanding the function to returns up to 40 scores in one lex-click!

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,7 @@
 ## code to prepare `DATASET` dataset goes here
 library(tidyverse)
-library(here)
 
-lextaledata <- read.csv(here('data-raw', 'lextaledata.csv')) # Read in csv file
+lextaledata <- read.csv(here::here('data-raw', 'lextaledata.csv')) # Read in csv file
 lextaledata$answer <- sample(c(0,1), replace = TRUE, 600) #generate new variable of binary random answers
 is.numeric(lextaledata$answer)
 usethis::use_data(lextaledata, overwrite = TRUE)
