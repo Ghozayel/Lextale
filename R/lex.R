1e65aa9c08303cdbfcd64ad25c9502b1c1590cdc
Ghozayel <gmo6606@gmail.com>
2022-09-16 11:02:36 +0100

    re: ID

--- R/lex.R
+++ R/lex.R
@@ -12,17 +12,18 @@ lex <- function(answerVar) {
   Data <-
     Lextale::lextale %>%
     #marking correct/incorrect entries (1=correct and 0=incorrect) in a new column called score
-    dplyr::mutate(score=if_else(answerVar==correct, 1, 0))
+    dplyr::mutate(score=if_else(answerVar$answer==correct, 1, 0),
+                  ID = answerVar$ID)
   accuracyByType <- Data %>%
     #calculate number of correct answers per type (word/ non-word) for each participant
-    dplyr::group_by(ids,type) %>%
+    dplyr::group_by(ID,type) %>%
     dplyr::summarise(N.correct=sum(score)) %>%
     #p.correct is the percentage for each type
     dplyr::mutate(p.correct=if_else(type=="word", N.correct/40*100, N.correct/20*100))
   print(accuracyByType)
   p.correctAV <-
     accuracyByType %>%
-    dplyr::group_by(ids) %>%
+    dplyr::group_by(ID) %>%
     #averaging p.correct of the two types to get the lextale score
     dplyr::summarise(p.correctAV=mean(p.correct))
   return(p.correctAV)
