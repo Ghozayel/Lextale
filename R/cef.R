230a7a11c2e1964ac6d5e6d92ae937e3bd6606b9
Ghozayel <gmo6606@gmail.com>
2022-10-10 12:21:18 +0100

    >=80, <60

--- R/cef.R
+++ R/cef.R
@@ -8,12 +8,12 @@ cef <- function(score) {
     score %>%
     #categorising scores into 3 CEF levels
     dplyr::mutate(CEF.level =
-                    ifelse(score<=80, "C1 & C2",
-                           ifelse(score>60 ,"B1 & lower",
+                    ifelse(score>=80, "C1 & C2",
+                           ifelse(score<60 ,"B1 & lower",
                                   "B2"))) %>%
     dplyr::mutate(CEF.desc =
-                    ifelse(score<=80, "Advanced/Proficient",
-                         ifelse(score>60, "Lower-intermediate & lower",
+                    ifelse(score>=80, "Advanced/Proficient",
+                         ifelse(score<60, "Lower-intermediate & lower",
                                 "Upper-intermediate")))
   return(score2cef)
 }
